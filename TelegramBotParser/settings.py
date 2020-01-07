943576cb8ed25de8b930b026308b3ddf129e8777
Shurgentum <k.asyeyev@gmail.cm>
2020-01-07 19:08:26 +0200

    registration templates

--- TelegramBotParser/settings.py
+++ TelegramBotParser/settings.py
@@ -125,11 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'TelegramBotParser', 'static')
-]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
