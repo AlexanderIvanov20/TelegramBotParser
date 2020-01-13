dc6ec4348babe140a431be7c99c630d350afda95
Shurgentum <k.asyeyev@gmail.cm>
2020-01-13 21:27:07 +0200

    liqpay deprecation

--- telegram_parser/liq_sdk/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-if sys.version_info >= (3, 0):
-    from .liqpay3 import *
-else:
-    from .liqpay import *
--- telegram_parser/views.py
+++ telegram_parser/views.py
@@ -2,7 +2,6 @@ from django.views import View
 from django.shortcuts import render, get_object_or_404
 from .models import TelegramParserComment
 from users.models import Profile
-from .liq_sdk import LiqPay
 
 
 class Index(View):
@@ -12,12 +11,13 @@ class Index(View):
 
         if 'page' in data:
             interval = 30 * int(data['page'])
-            comments = TelegramParserComment.objects.all()[interval - 30:interval]
+            comments = TelegramParserComment.objects.all()[
+                interval - 30:interval]
         context = {
             'title': 'Комментарии',
             'comments': comments
         }
-        # pages = 
+        # pages =
         return render(request, 'parser/mainpage.html', context)
 
     def post(self, request):
@@ -54,20 +54,7 @@ class Payment(View):
             profile = get_object_or_404(Profiles, id_user=data.get('id_user'))
         else:
             pass
-        liqpay = LiqPay(
-            'sandbox_i41551166851',
-            'sandbox_TQuWlo3gIC9Vz9fchAq9qrdPGJz3segJzpNpD1N7'
-        )
-        form = liqpay.cnb_form({
-            "action": "hold",
-            "amount": "99",
-            "currency": "UAH",
-            "description": f"Premium account(1 month) for user",
-            "order_id": "order_id_2",
-            "version": "3"
-        })
         context = {
-            'title': 'Оплата',
-            'form': form
+            'title': 'Оплата'
         }
         return render(request, 'paymentpage.html', context)
