40533ac15ec712752e0582accc0a05959e35087d
Shurgentum <k.asyeyev@gmail.com>
2020-01-15 00:09:56 +0200

    search template & basic functionality

--- telegram_parser/urls.py
+++ telegram_parser/urls.py
@@ -3,7 +3,7 @@ from .views import Index, detailed, Profiles, Payment
 
 
 urlpatterns = [
-    path('', Index.as_view(), name='index'),
+    path('<page>', Index.as_view(), name='index'),
     path('detailed/<comment_id>', detailed, name='detailed'),
     path('profiles', Profiles.as_view(), name='profiles'),
     path('payment', Payment.as_view(), name='payment')
--- telegram_parser/views.py
+++ telegram_parser/views.py
@@ -2,25 +2,58 @@ from django.views import View
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import TelegramParserComment
 from users.models import Profile
+from django.contrib import messages
 
 
 class Index(View):
-    def get(self, request):
-        data = dict(request.GET)
+    def get(self, request, page=1):
+        data = request.GET
+        page = int(page)
+        filter_data = {key: item for key, item in data.items() if item != ''}
+        fill_data = filter_data.copy()
         comments = TelegramParserComment.objects.all()
-        pagelist = list(range(1, int(len(comments) / 30)))
-        # Pagination
-        if 'page' in data:
-            interval = 30 * int(data.get('page')[0])
-            comments = comments[interval - 30:interval]
-        else:
-            comments = comments[:30]
-            data['page'] = 1
 
+        if 'search' in data and len(data.get('search')) > 0:
+            search = filter_data.pop('search')
+            page = 1
+            try:
+                searchtype = filter_data.pop('searchtype')
+                if searchtype == 'short':
+                    comments = comments.filter(
+                        short__contains=data.get('search')
+                    )
+                # elif False:
+                #     # !TODO
+                #     pass
+                # elif False:
+                #     # !TODO
+                #     pass
+                # ! Yet unfunctional
+                # filter_data = {key + '__contains': item for key, item in filter_data.items()}
+                # comments = comments.filter(
+                #     **{searchtype: search}
+                # )
+            except KeyError as keyerr:
+                print(keyerr)
+                messages.info(request, 'Тип поиска не указан')
+        elif 'search' not in data and 'searchtype' in data:
+            messages.info(request, 'Пустая строка поиска')
+
+        if len(filter_data) > 0:
+            comments = comments.filter(**filter_data)
+        pagelist = list(range(1, int(len(comments) / 30)))
+
+        # Applying page interval
+        if len(comments) > 30:
+            comments = comments[30 * page-30:30 * page]
+
+        if len(comments) == 0:
+            messages.warning(request, 'Отзывов не найдено')
         context = {
             'title': 'Комментарии',
             'comments': comments,
-            'pagelist': pagelist
+            'pagelist': pagelist,
+            'filldata': fill_data
         }
         return render(request, 'parser/mainpage.html', context)
 
@@ -31,6 +64,35 @@ class Index(View):
         }
         return render(request, 'parser/mainpage.html', context)
 
+# ! Deprecated, left for info purposes
+    # def deprecated(self, request):
+    #     data = dict(request.GET)
+    #     print(data)
+    #     data = {key: item[0] for key, item in data.items() if item != ['']}
+    #     print(data)
+    #     comments = TelegramParserComment.objects.filter(**data)
+    #     pagelist = list(range(1, int(len(comments) / 30)))
+
+    #     # Pagination
+    #     if 'page' in data:
+    #         interval = 30 * int(data.get('page')[0])
+
+    #         try:
+    #             comments = comments[interval - 30:interval]
+    #         except Exception as e:
+    #             comments = comments[:30]
+    #             data['page'] = 1
+    #     else:
+    #         comments = comments[:30]
+    #         data['page'] = 1
+
+    #     context = {
+    #         'title': 'Комментарии',
+    #         'comments': comments,
+    #         'pagelist': pagelist
+    #     }
+    #     return render(request, 'parser/mainpage.html', context)
+    
 
 def detailed(request, comment_id):
     comment = TelegramParserComment.objects.get(id=int(comment_id))
