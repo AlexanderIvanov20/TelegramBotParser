b82752f6ae33a1b914e8eeb7ad68850a893a3224
Shurgentum <k.asyeyev@gmail.com>
2020-01-15 22:20:06 +0200

    no page specified fix

--- telegram_parser/urls.py
+++ telegram_parser/urls.py
@@ -3,6 +3,7 @@ from .views import Index, detailed, Profiles, Payment
 
 
 urlpatterns = [
+    path('', Index.as_view(), name='index_1'),
     path('<page>', Index.as_view(), name='index'),
     path('detailed/<comment_id>', detailed, name='detailed'),
     path('profiles', Profiles.as_view(), name='profiles'),
