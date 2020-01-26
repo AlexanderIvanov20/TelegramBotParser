e3a9a0cc5bb88bdef612b4d0ea6b99fe05a265d6
Shurgentum <k.asyeyev@gmail.com>
2020-01-26 21:44:18 +0200

    additional config messages

--- telegram_parser/forms.py
+++ telegram_parser/forms.py
@@ -2,5 +2,9 @@ from django import forms
 
 class ConfigForm(forms.Form):
     greeting = forms.CharField(widget=forms.Textarea)
+    cooldown = forms.CharField(widget=forms.Textarea)
+    search = forms.CharField(widget=forms.Textarea)
+    error = forms.CharField(widget=forms.Textarea)
+    misunderstanding = forms.CharField(widget=forms.Textarea)
     token = forms.CharField(label='Bot Token')
     provider_token = forms.CharField(label='Payment provider token')
