d644864cd78f35d3f3f6076d1e17e1228e758298
Shurgentum <k.asyeyev@gmail.com>
2020-01-27 14:57:03 +0200

    additional response fields

--- telegram_parser/forms.py
+++ telegram_parser/forms.py
@@ -1,10 +1,15 @@
 from django import forms
 
+
 class ConfigForm(forms.Form):
     greeting = forms.CharField(widget=forms.Textarea)
     cooldown = forms.CharField(widget=forms.Textarea)
     search = forms.CharField(widget=forms.Textarea)
     error = forms.CharField(widget=forms.Textarea)
     misunderstanding = forms.CharField(widget=forms.Textarea)
+    choose_tab = forms.CharField(widget=forms.Textarea)
+    choose_company = forms.CharField(widget=forms.Textarea)
+    not_found = forms.CharField(widget=forms.Textarea)
+    try_again = forms.CharField(widget=forms.Textarea)
     token = forms.CharField(label='Bot Token')
     provider_token = forms.CharField(label='Payment provider token')
